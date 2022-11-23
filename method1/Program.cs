b5f5c9bed75b9784d0d44347cca5c028b23ff61b
Lutfu24 <lutfuhm@code.edu.az>
2022-11-23 17:58:13 +0400

    method

---
+++ method1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace method1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Method();
+        }
+        static void Method ()
+        {
+           string str = "bU tasKi metHodla yazMalisiZ";
+
+            TextInfo info = new CultureInfo("en-En", false).TextInfo;
+            string result = info.ToTitleCase(str);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
